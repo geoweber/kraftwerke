c5aaa74b5853e34b4bc87f8fe681ae1280280c0c
Georg Weber <geo.weber@t-online.de>
2020-04-02 22:00:45 +0200

    Initial Project Add

---
+++ src/main/java/com/vantago/kraftwerke/KraftwerkeApplication.java
@@ -0,0 +1,13 @@
+package com.vantago.kraftwerke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraftwerkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraftwerkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vantago/kraftwerke/KraftwerkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantago.kraftwerke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraftwerkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
